d34cfe3247659c020f2a37e93f6c1fa632ee065f
KKleinbeck <kekleinbeck@gmail.com>
2019-02-09 16:08:21 +0100

    Updated more errors in the script

--- dev/bots/travis_script.sh
+++ dev/bots/travis_script.sh
@@ -2,14 +2,10 @@
 set -ex
 
 export PATH=~/development/flutter/bin:$PATH
-export ROOT="$PWD"
 
 if [[ "$SHARD" == "dartfmt" ]]; then
  echo 'Formating code'
- cd $ROOT/package
  flutter format . || exit $?
 else
-  # tests shard
-  cd $ROOT/package
   flutter --no-color test --machine --start-paused test/titled_page_view_test.dart || exit $?
 fi
